187ab2a058dc5743450f7fa970485733cd4598d4
christof <christof.sirk@gmail.com>
2018-09-09 18:57:13 +0200

    Add algebraic data type LispVal.

--- parser.hs
+++ parser.hs
@@ -1,5 +1,14 @@
-import Text.ParserCombinators.Parsec hiding (spaces)
-import System.Environment
+import           System.Environment
+import           Text.ParserCombinators.Parsec hiding (spaces)
+
+data LispVal
+  = Atom String
+  | List [LispVal]
+  | DottedList [LispVal]
+               LispVal
+  | Number Integer
+  | String String
+  | Bool Bool
 
 spaces :: Parser ()
 spaces = skipMany1 space
@@ -8,9 +17,10 @@ symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
-  Left err -> "No match: " ++ show err
-  Right val -> "Found value: " ++ show val
+readExpr input =
+  case parse (spaces >> symbol) "lisp" input of
+    Left err  -> "No match: " ++ show err
+    Right val -> "Found value: " ++ show val
 
 main :: IO ()
 main = do
