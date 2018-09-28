30cd12d6a2d79e994971c4efb9e7e11cab1309c6
christof <christof.sirk@gmail.com>
2018-09-28 19:50:59 +0200

    Implement Show for LispError.

--- parser.hs
+++ parser.hs
@@ -31,6 +31,20 @@ data LispError
                String
   | Default String
 
+showError :: LispError -> String
+showError (NumArgs expected found) =
+  "Expected " ++ show expected ++ " args; found values " ++ unwordsList found
+showError (TypeMismatch expected found) =
+  "Invalid type: expected " ++ expected ++ ", found " ++ show found
+showError (Parser parseErr) = "Parse error at " ++ show parseErr
+showError (BadSpecialForm message form) = message ++ ": " ++ show form
+showError (NotFunction message func) = message ++ ": " ++ show func
+showError (UnboundVar message varname) = message ++ ": " ++ varname
+showError (Default message) = "Error: " ++ message
+
+instance Show LispError where
+  show = showError
+
 spaces :: Parser ()
 spaces = skipMany1 space
 
