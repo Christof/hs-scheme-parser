a9a09ced2024fdbc36b525c033e2858668dbad91
christof <christof.sirk@gmail.com>
2018-09-29 20:25:35 +0200

    Implement eq? and eqv?, e.g. (eq? '(3 2 1) '(3 2 2))

--- parser.hs
+++ parser.hs
@@ -335,6 +335,23 @@ cons [x, DottedList xs xlast] = return $ DottedList (x : xs) xlast
 cons [x1, x2]                 = return $ DottedList [x1] x2
 cons badArgList               = throwError $ NumArgs 2 badArgList
 
+eqv :: [LispVal] -> ThrowsError LispVal
+eqv [(Bool arg1), (Bool arg2)] = return $ Bool $ arg1 == arg2
+eqv [(Number arg1), (Number arg2)] = return $ Bool $ arg1 == arg2
+eqv [(String arg1), (String arg2)] = return $ Bool $ arg1 == arg2
+eqv [(Atom arg1), (Atom arg2)] = return $ Bool $ arg1 == arg2
+eqv [(DottedList xs x), (DottedList ys y)] =
+  eqv [List $ xs ++ [x], List $ ys ++ [y]]
+eqv [(List arg1), (List arg2)] =
+  return $ Bool $ (length arg1 == length arg2) && (all eqvPair $ zip arg1 arg2)
+  where
+    eqvPair (x1, x2) =
+      case eqv [x1, x2] of
+        Left _err        -> False
+        Right (Bool val) -> val
+eqv [_, _] = return $ Bool False
+eqv badArgList = throwError $ NumArgs 2 badArgList
+
 primitives :: [(String, [LispVal] -> ThrowsError LispVal)]
 primitives =
   [ ("+", numericBinop (+))
@@ -369,6 +386,8 @@ primitives =
   , ("car", car)
   , ("cdr", cdr)
   , ("cons", cons)
+  , ("eq?", eqv)
+  , ("eqv?", eqv)
   ]
 
 apply :: String -> [LispVal] -> ThrowsError LispVal
