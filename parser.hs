4cf5c09b404f730c34bab89d1a81e20e0a2c4288
christof <christof.sirk@gmail.com>
2018-09-29 20:05:12 +0200

    Add support for if expression.
    E.g. "(if (= 3 2) \"a\" \"b\")"

--- parser.hs
+++ parser.hs
@@ -362,6 +362,11 @@ eval val@(Number _) = return val
 eval val@(Float _) = return val
 eval val@(Bool _) = return val
 eval val@(Vector _) = return val
+eval (List [Atom "if", pred, conseq, alt]) = do
+  result <- eval pred
+  case result of
+    Bool False -> eval alt
+    _otherwise -> eval conseq
 eval (List [Atom "quote", val]) = return val
 eval (List (Atom func:args)) = mapM eval args >>= apply func
 eval val@(List _) = return val
